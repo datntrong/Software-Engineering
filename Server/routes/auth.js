21ea92226c53a1865506392504f696cee01ea3f9
haihy2001 <hai2001hy@gmail.com>
2021-05-05 15:55:19 +0700

    gui request len server

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const {check} = require('express-validator');
-const path = require('path');
 const auth = require('../middleware/auth');
 const authController = require('../controllers/auth');
 const router = express.Router();
@@ -9,15 +8,12 @@ const router = express.Router();
  *  @desc       log in user
  *  @access     Private
  */
-router.get("/login",(req,res)=>{
-  res.sendFile(path.join(__dirname + '/dangnhap.html'));
-})
 router.post(
   '/',
-  // [
-  //   check('username', 'Please include a valid username').isLength({min: 3}),
-  //   check('password', 'Password is required').not().isEmpty(),
-  // ],
+  [
+    check('username', 'Please include a valid username').isLength({min: 3}),
+    check('password', 'Password is required').not().isEmpty(),
+  ],
   authController.login
 );
 
--- Server/routes/index.route.js
+++ Server/routes/index.route.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 const auth = require('./auth');
 const users = require('./users');
 const trips = require('./trips');
 
 const router = express.Router();
+
+router.get("/login",(req,res)=>{
+  res.sendFile(path.join(__dirname + '/dangnhap.html'));
+})
 
 router.use('/auth', auth);
 router.use('/users', users);
